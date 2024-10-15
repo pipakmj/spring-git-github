be8667bf0973c27de2644a42f4baf3aea4e00212
pipakmj <k30327@naver.com>
2024-10-15 14:30:13 +0900

    1-th commit

---
+++ src/main/java/com/pipasts/sgg/SpringGitGithubApplication.java
@@ -0,0 +1,13 @@
+package com.pipasts.sgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipasts/sgg/SpringGitGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipasts.sgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
