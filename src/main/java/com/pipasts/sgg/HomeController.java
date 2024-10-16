633186ad59ea608f11de84efc2e7b4a91fc85d58
pipakmj <k30327@naver.com>
2024-10-16 11:08:52 +0900

    1108

--- src/main/java/com/pipasts/sgg/HomeController.java
+++ src/main/java/com/pipasts/sgg/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String Home() {
-		return "Hello World<br> Success..202410161027";
+		return "AWS server pull test...";
 	}
 }
