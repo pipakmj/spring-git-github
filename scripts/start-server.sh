17e3aa05a124e54bac80e1eff4dd23a0d8a47611
pipakmj <k30327@naver.com>
2024-10-17 14:53:40 +0900

    171453

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/spring-git-github
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop spring-git-github || true
+docker rm spring-git-github || true
+docker pull 940482417952.dkr.ecr.ap-northeast-2.amazonaws.com/spring-git-github/spring-git-github:latest
+docker run -d --name spring-git-github -p 8080:8080 940482417952.dkr.ecr.ap-northeast-2.amazonaws.com/spring-git-github/spring-git-github:latest
 echo "--------------- 서버 배포 끝 -----------------"
