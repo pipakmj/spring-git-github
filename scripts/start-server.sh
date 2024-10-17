aba4af77aba66f62bd30e7d0202898ec164b3628
pipakmj <k30327@naver.com>
2024-10-17 15:14:43 +0900

    171514

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop spring-git-github || true
 docker rm spring-git-github || true
-docker pull 940482417952.dkr.ecr.ap-northeast-2.amazonaws.com/spring-git-github/spring-git-github:latest
-docker run -d --name spring-git-github -p 8080:8080 940482417952.dkr.ecr.ap-northeast-2.amazonaws.com/spring-git-github/spring-git-github:latest
+docker pull ${{ steps.login-ecr.outputs.registry }}/spring-git-github/spring-git-github:latest
+docker run -d --name spring-git-github -p 8080:8080 ${{ steps.login-ecr.outputs.registry }}/spring-git-github/spring-git-github:latest
 echo "--------------- 서버 배포 끝 -----------------"
