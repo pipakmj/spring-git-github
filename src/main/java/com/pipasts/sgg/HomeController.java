15867627d762f8153c391fde9cede26e1e032f81
pipakmj <k30327@naver.com>
2024-10-18 09:43:34 +0900

    180943

--- src/main/java/com/pipasts/sgg/HomeController.java
+++ src/main/java/com/pipasts/sgg/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String Home() {
-		return "CICD Working sts-project..:::: docker test 02";
+		return "CICD Working sts-project..:::: code-deploy test 01..";
 	}
 }
